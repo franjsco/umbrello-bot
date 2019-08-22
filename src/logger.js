61467f2f8a3a6e4d1c85e30352e35ba9c7efb861
Francesco Esposito <33671357+frsposito@users.noreply.github.com>
2019-08-22 17:30:30 +0200

    Add logger

---
+++ src/logger.js
@@ -0,0 +1,27 @@
+import winston from 'winston';
+
+const { format } = winston;
+
+
+const customFormat = format.printf(({
+  level,
+  message,
+  label,
+  timestamp,
+}) => `${timestamp} [${label}] ${level}: ${message}`);
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: format.combine(
+    format.label({ label: 'umbrello-bot' }),
+    format.timestamp(),
+    customFormat,
+  ),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+export default logger;
